0bed0c7557080bb9275d875e4ea0f2b1fdaf69e2
Victoria Curl <victoriacurl9@gmail.com>
2019-12-18 00:43:14 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -9,12 +9,13 @@ array.sort
 end
 
 def using_reverse(array)
-  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
 array.reverse
 end
 
 def using_first(array)
-
+array = ["wow", "I", "am", "really", "learning", "arrays!"]
+array.first
 end
 
 def using_last(array)
